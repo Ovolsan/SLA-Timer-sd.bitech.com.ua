f1a553cea50e27c153055826bd5036595e13da5b
Ovolsan <30950675+Ovolsan@users.noreply.github.com>
2026-06-07 22:07:43 +0300

    Update URLs for user script download and update

--- SLA Timer sd.bitech.com.ua.user.js
+++ SLA Timer sd.bitech.com.ua.user.js
@@ -6,8 +6,8 @@
 // @author       Ovolya
 // @match        *://sd.bitech.com.ua/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bitech.com.ua
-// @updateURL    https://github.com/Ovolsan/SLA-Timer-sd.bitech.com.ua/raw/refs/heads/main/SLA%20Timer%20sd.bitech.com.ua.js
-// @downloadURL  https://github.com/Ovolsan/SLA-Timer-sd.bitech.com.ua/raw/refs/heads/main/SLA%20Timer%20sd.bitech.com.ua.js
+// @updateURL    https://github.com/Ovolsan/SLA-Timer-sd.bitech.com.ua/raw/refs/heads/main/SLA%20Timer%20sd.bitech.com.ua.user.js
+// @downloadURL  https://github.com/Ovolsan/SLA-Timer-sd.bitech.com.ua/raw/refs/heads/main/SLA%20Timer%20sd.bitech.com.ua.user.js
 // @grant        none
 // ==/UserScript==
 
